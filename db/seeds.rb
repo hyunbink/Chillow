6146beabe5876c335a864d85333958440730d962
Matia Hyunbin Kim <matia.h.kim@gmail.com>
2022-05-16 17:17:12 -0700

    seed notes for listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require 'open-uri'
+
 User.destroy_all
 User.create(email: 'demologin@aa.io', password: 'password')
+
+
+# list1 = Listing.create(schema)
+# list1.photos.attch(io: open("url goes here, given by aws, don't have yet"), filename:'name_of_file.png/jpg')
+# list1.photos.attch(io: open("url goes here, given by aws, don't have yet"), filename:'name_of_file.png/jpg')
+# list1.photos.attch(io: open("url goes here, given by aws, don't have yet"), filename:'name_of_file.png/jpg')
+# list1.photos.attch(io: open("url goes here, given by aws, don't have yet"), filename:'name_of_file.png/jpg')
