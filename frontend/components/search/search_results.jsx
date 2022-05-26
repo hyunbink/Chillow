ec65ba4fbe8d518339dc6a67ca8b068d5abddef0
Matia Hyunbin Kim <matia.h.kim@gmail.com>
2022-05-26 12:11:03 -0700

    search results rendering

--- frontend/components/search/search_results.jsx
+++ frontend/components/search/search_results.jsx
@@ -3,17 +3,57 @@ import React from "react";
 class SearchResults extends React.Component{
     constructor(props){
         super(props);
+        console.log('listings', this.props.listings)
+
     };
 
     componentDidMount(){
-        
+        let that = this;
+        if (this.props.saves) {
+            this.props.saves.forEach(element => {
+                // that.props.fetchListing(element[1].listing_id);
+            });
+        }        
     }
 
     render(){
+        if (!this.props.listings) return null;
+        let listingsArr = Object.values(this.props.listings)
         return(
-            <div>
-                Showing Results for query
-                
+            <div className="index-div-right">
+                <div key='listings' className="index-all-listings">
+                    {listingsArr.map((listing,j)=>(
+                            <div key={j} className="index-listing" onClick={()=>this.props.openModal('show',  listing.id)} >
+                                <div className="index-listing-photo-div">
+                                    <img className="index-listing-photos" 
+                                    src={listing.photoUrls[0]} 
+                                    alt="listing main photo">
+                                    </img>
+                                </div>
+                                <div className="index-listing-info">
+                                    <div className="listing-price-div">
+                                        <span className="listing-price">
+                                            ${listing.price.toLocaleString(undefined, { minimumFractionDigits: 0 })}
+                                        </span>
+                                    </div>
+                                    <div className="index-listing-char-info">
+                                        <span className="listing-physical-info">{listing.beds} </span>
+                                        <span className="listing-info">bd | </span>
+                                        <span className="listing-physical-info">{listing.baths} </span>
+                                        <span className="listing-info">ba | </span>
+                                        <span className="listing-physical-info">{listing.sqft.toLocaleString(undefined, { minimumFractionDigits: 0 })} </span>
+                                        <span className="listing-info">sqft</span>
+                                    </div>
+                                    <div className="listing-address-div">
+                                        <span className="listing-address">{listing.street}, </span>
+                                        <span className="listing-address">{listing.city}, </span>
+                                        <span className="listing-address">{listing.state} </span>
+                                        <span className="listing-address">{listing.zip_code}</span>
+                                    </div>
+                                </div>
+                            </div>
+                    ))}
+                </div>
             </div>
         )
     }
--- frontend/components/search/search_results_container.jsx
+++ frontend/components/search/search_results_container.jsx
@@ -1,16 +1,20 @@
 import { connect } from "react-redux";
 import SearchResults from "./search_results";
 import { fetchAllListings, search } from "../../actions/listing_actions";
+import { createSave, deleteSave } from "../../actions/save_actions";
 
 const mSTP = state => {
     console.log('SRL', state)
     return {
-        listings: state.entities.listings
+        listings: state.entities.listings,
+        saves: Object.entries(state.entities.saves),
     }
 };
 
 const mDTP = dispatch => ({
-    search: query => dispatch(search(query))
+    search: query => dispatch(search(query)),
+    deleteSave: save => dispatch(deleteSave(save)),
+    createSave: save => dispatch(createSave(save))
 });
 
 export default connect(mSTP, mDTP)(SearchResults);
