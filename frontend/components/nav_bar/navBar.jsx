75032e8e7873bd2d599bc6eddb49f69741b2150d
Matia Hyunbin Kim <matia.h.kim@gmail.com>
2022-05-26 17:44:09 -0700

    splash styling

--- frontend/components/nav_bar/navBar.jsx
+++ frontend/components/nav_bar/navBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
+import { FaUserCircle } from 'react-icons/fa';
 
 const NavBar = ({ currentUser, logout }) => {
     const sessionLinks = () => (
@@ -31,14 +33,15 @@ const NavBar = ({ currentUser, logout }) => {
                 <img src={window.logo} alt="chillow-logo"></img>
             </Link>
             <div className='nav-right'>
-            <Link id='logout' onClick={logout} to="/">Logout</Link>
+            <a className='nav-left-link' href='https://github.com/hyunbink/Chillow' >Github</a>
+            <a className='nav-left-link' href='https://www.linkedin.com/in/matia-kim/' >LinkedIn</a>
             <span id='logged-in-user'>
-                <img id='user-symbol' src={window.userSymbol} alt='user-symbol'/>
-                {currentUser.email}
+                {/* <FaUserCircle id='href-logo'/> */}
+                    <div className='nav-left-link'>{currentUser.email}</div>
                 <div className="drop-down-box">
                     <div className='drop-down-saves-link'><Link to={`/listings/saves`}>Saved Listings</Link></div>
-                    <div className='drop-down-saves-link'><Link id='#' onClick={logout} to="/">Your Listings</Link></div>
-                    <div className='drop-down-saves-link'><Link id='logout' onClick={logout} to="/">Logout</Link></div>
+                    <div className='drop-down-saves-link'><Link to="/listings/user/listings">Your Listings</Link></div>
+                    <div className='drop-down-saves-link'><Link  onClick={logout} to="/">Logout</Link></div>
                 </div>
                 </span>
             </div>
@@ -50,3 +53,6 @@ const NavBar = ({ currentUser, logout }) => {
 };
 
 export default NavBar;
+
+{/* <BsGithub className='href-logo'/>
+<BsLinkedin className='href-logo'/> */}
