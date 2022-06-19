b133b455fb5b23e96d2ff75b7bf74c64c0749530
Matia Hyunbin Kim <matia.h.kim@gmail.com>
2022-06-18 18:15:57 -0700

    map to render default long/lat case

--- frontend/components/listings_map/listings_map.jsx
+++ frontend/components/listings_map/listings_map.jsx
@@ -22,10 +22,24 @@ class ListingMap extends React.Component{
     }
     
     componentDidUpdate() {
-        this.mapOptions = {
-                    center: { lat: this.props.listings[1].latitude, lng: this.props.listings[1].longitude },   
-                    zoom: 10
-                };
+        console.log('listings map props', this.props.listings)
+        console.log('object attempt map props', Object.values(this.props.listings))
+        let allListingsArr = Object.values(this.props.listings)
+        console.log('first listing', allListingsArr[0])
+        console.log('all listings from map', allListingsArr)
+        console.log("map lat and long", allListingsArr[0].latitude, allListingsArr[0].longitude)
+        let that = this;
+        if (allListingsArr[0].longitude) {
+                that.mapOptions = {
+                            center: { lat: allListingsArr[0].latitude, lng: allListingsArr[0].longitude },   
+                            zoom: 10
+                        };
+        } else {
+            that.mapOptions = {
+                center: { lat: 37.6, lng: -121.5 },   
+                zoom: 8
+            };
+        }
         this.map = new google.maps.Map(this.mapNode, this.mapOptions);    // this renders map
         this.MarkerManager = new MarkerManager(this.map, this.props.openModal);
         this.MarkerManager.updateMarkers(Object.values(this.props.listings));
